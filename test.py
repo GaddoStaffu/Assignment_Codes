7b97f9265264e1012bcd53015feccd2e3605c3a9
GaddoStaffu <jomarilagumen02@gmail.com>
2024-05-09 12:44:12 +0800

    updated print statement

--- test.py
+++ test.py
@@ -18,3 +18,6 @@ employee1 = Employee('Person1', 34, 'Manager', 'Address1')
 employee2 = Employee('Person2', 30, 'Assistant Manager', 'Address2')
 employee3 = Employee('Person3', 25, 'Cashier', 'Address3')
 
+print(employee1.description())
+print(employee2.description())
+print(employee3.description())
