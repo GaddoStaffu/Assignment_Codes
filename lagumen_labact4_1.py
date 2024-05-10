9f776719b11094c8a96cfb0f4f923f1fc245e3f3
GaddoStaffu <jomarilagumen02@gmail.com>
2024-05-10 22:37:40 +0800

    fgd

--- lagumen_labact4_1.py
+++ lagumen_labact4_1.py
@@ -2,19 +2,17 @@ from threading import Thread
 import time
 
 
-def print_numbers(start, end):
-    for x in range(start, end + 1):
+def print_numbers(starting, ending):
+    for x in range(starting, ending+1):
         print(x)
         time.sleep(0.1)  
 
 def main():
 
-    starting = 1
-    ending = 50
 
 
-    thread1 = Thread(target=print_numbers, args=(starting, ending//2))
-    thread2 = Thread(target=print_numbers, args=(ending//2 + 1, ending))
+    thread1 = Thread(target=print_numbers, args=(1, 25))
+    thread2 = Thread(target=print_numbers, args=(26, 50))
 
 
     thread1.start()
